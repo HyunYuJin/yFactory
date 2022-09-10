d85aa1af0fa7436a8566b5d453cf3b205ef3a1c1
HyeonYuJin <gusdbwls9772@gmail.com>
2022-09-10 21:28:26 +0900

    feat: Dot 캔버스 생성, 마우스 움직임에 따른 위치 업데이트 기능 추가

--- CursorEffects/src/dot.js
+++ CursorEffects/src/dot.js
@@ -1,32 +1,59 @@
+let width = window.innerWidth
+let height = window.innerHeight
+const cursor = { x: width / 2, y: height / 2 }
+const position = { x: width / 2, y: height / 2 }
+let canvas, context
+
 const Dot = (el) => {
   const element = el || document.body
-  console.log(element)
-  const width = window.innerWidth
-  const height = window.innerHeight
-  const cursor = { x: width / 2, y: height / 2 }
-  let dot = getDot(width / 2, height / 2, 10, 10)
-  let canvas, context
-
-  // dot.move(cursor.x, cursor.y, context)
+  createCanvas(element)
+  cursorStyle()
+ 
+  element.addEventListener('mousemove', onMousemove)
 }
 
-const getDot = (x, y, width, lag) => {
-  const position = { x, y }
+const onMousemove = (event) => {
+  cursor.x = event.clientX
+  cursor.y = event.clientY
 
-  const move = (x, y, context) => {
-    position.x += (x - position.x) / lag
-    position.y += (y - position.y) / lag
+  updateCursor()
+}
 
-    dotContext(context)
+const updateCursor = () => {
+  context.clearRect(0, 0, canvas.width, canvas.height)
+  cursorMove(cursor.x, cursor.y)
+}
+
+const createCanvas = (element) => {
+  canvas = document.createElement('canvas')
+  context = canvas.getContext('2d')
+
+  if (element) {
+    canvas.style.position = 'absolute'
+    element.appendChild(canvas)
+    canvas.width = element.clientWidth
+    canvas.height = window.innerHeight
+  } else {
+    canvas.style.position = 'relative'
+    element.appendChild('body')
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
   }
+}
 
-  const dotContext = () => {
-    context.fillStyle = `rgba(50, 50, 50, 0.65)`
-    context.beginPath()
-    context.arc(position.x, position.y, width, 0, 2 * Math.PI)
-    context.fill()
-    context.closePath()
-  }
+const cursorStyle = () => {
+  context.fillStyle = `rgba(50, 50, 50, 0.65)`
+  context.beginPath()
+  context.arc(position.x, position.y, 10, 0, 2 * Math.PI)
+  context.fill()
+  context.closePath()
+}
+
+const cursorMove = (x, y) => {
+  position.x = x
+  position.y = y
+
+  cursorStyle()
 }
 
 export default Dot
