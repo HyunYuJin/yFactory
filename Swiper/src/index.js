87846701312e047c913b55c3b4a6c2ba0dfd52f7
HyeonYuJin <gusdbwls9772@gmail.com>
2022-09-15 21:10:58 +0900

    feat: 무한스크롤 구현

--- Swiper/src/index.js
+++ Swiper/src/index.js
@@ -4,33 +4,37 @@ import { extend } from './utils.js'
 class Swiper {
   constructor (element, config) {
     this.config = extend(params, config)
-    this.nodes = {}
-    this.nodes.element = element
-    this.nodes.navigation = this.config.navigation
-    this.nodes.slides = Array.from(this.nodes.element.querySelectorAll('.swiper__item'))
-    this.nodes.wrapper = this.nodes.element.querySelector('.swiper__wrapper') || this._getWrapper()
-
+    this.nodes = {
+      element: element,
+      wrapper: element.querySelector('.swiper__wrapper'),
+      slides: Array.from(element.querySelectorAll('.swiper__item')),
+      navigation: {
+        prevEl: this.config.navigation.prevEl,
+        nextEl: this.config.navigation.nextEl
+      }
+    }
+    
     this.currentIndex = 0
-    this.translateX = 0
-    this.timer
-    this.isFirstSlide = true
+    this.firstSlide = this.nodes.wrapper.firstElementChild
+    this.lastSlide = this.nodes.wrapper.lastElementChild
 
     this._init()
   }
 
   get slideWidth () {
-    return this.nodes.element.clientWidth
+    return 100 / this.nodes.slides.length
+  }
+
+  get slideWidthTotal () {
+    return 100 * this.nodes.slides.length
   }
 
   get slideTotalIndex () {
-    return this.nodes.slides.length
-  }
-  
-  get slideTotalWidth () {
-    return this.slideWidth * this.slideTotalIndex
+    return this.nodes.slides.length - 1
   }
 
   _init () {
+    this._initStyles()
     this._initEvents()
 
     if (this.config.autoplay?.delay) {
@@ -40,26 +44,56 @@ class Swiper {
     }
   }
 
+  _initStyles () {
+    this.nodes.wrapper.style.width = this.slideWidthTotal + '%'
+    this.nodes.slides.forEach(slide => {
+      slide.style.flexBasis = this.slideWidth + '%'
+    })
+  }
+
   _initEvents () {
     this.nodes.navigation.prevEl?.addEventListener('click', this._navigate.bind(this, 'prev'))
     this.nodes.navigation.nextEl?.addEventListener('click', this._navigate.bind(this, 'next'))
+    this.nodes.wrapper.addEventListener('transitionend', () => {
+      if (this.direction === 'prev') {
+        this.nodes.wrapper.prepend(this.nodes.wrapper.lastElementChild)
+      } else if (this.direction === 'next') {
+        this.nodes.wrapper.append(this.nodes.wrapper.firstElementChild)
+
+        // 첫번째 슬라이드고 처음 슬라이드 시작하면 맨 앞에 슬라이드 한장 붙이기
+      }
+
+      this.nodes.wrapper.style.transition = 'none'
+      this.nodes.wrapper.style.transform = 'translateX(0%)'
+      setTimeout(() => {
+        this.nodes.wrapper.style.transition = 'all 0.5s'
+      })
+    }, false)
   }
 
   _navigate (direction) {
+    this.direction = direction
+
     if (direction === 'prev') {
-      this.currentIndex = this.currentIndex > 0 ? --this.currentIndex : this.slideTotalIndex - 1
+      this.currentIndex = this.currentIndex > 0 ? --this.currentIndex : this.slideTotalIndex
     } else if (direction === 'next') {
-      this.currentIndex = this.currentIndex < this.slideTotalIndex - 1 ? ++this.currentIndex : 0
-      this.translateX += this.isFirstSlide ? 200 : 100
+      this.currentIndex = this.currentIndex < this.slideTotalIndex ? ++this.currentIndex : 0
     }
 
-    this._animate()
+    this.translateX = this.slideWidth
+
+    this._animate(direction)
     this._setInfiniteSwipe(direction)
   }
 
-  _animate () {
+  _animate (direction) {
     this.nodes.wrapper.style.transition = '0.5s'
-    this.nodes.wrapper.style.transform = `translateX(-${this.translateX}%)`
+
+    if (direction === 'prev') {
+      this.nodes.wrapper.style.transform = `translateX(${this.translateX}%)`
+    } else if (direction === 'next') {
+      this.nodes.wrapper.style.transform = `translateX(-${this.translateX}%)`
+    }
   }
 
   _setInfiniteSwipe (direction) {
