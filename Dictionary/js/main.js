159edaeb94889bb8c93aefe31f0d8ff6d19477b6
HyeonYuJin <gusdbwls9772@gmail.com>
2022-09-29 23:35:24 +0900

    feat: 이벤트 리스너에 따른 처리 및 오디오 설정

--- Dictionary/js/main.js
+++ Dictionary/js/main.js
@@ -10,6 +10,55 @@ const nodes = {
   synonyms: document.querySelector('.synonyms'),
   cancel: document.querySelector('.cancel')
 }
+let audio
+
+function init () {
+  initEvents()
+}
+
+function initEvents () {
+  nodes.input.addEventListener('keyup',  (event) => {
+    handleKeyUp(event)
+  })
+  nodes.input.addEventListener('change', (event) => {
+    handleChange(event)
+  })
+  nodes.volumn.addEventListener('click', (event) => {
+    handleVolumn(event)
+  })
+  nodes.cancel.addEventListener('click', (event) => {
+    handleRemove(event)
+  })
+}
+
+function handleKeyUp (event) {
+  const { target, key } = event
+  const word = target.value.replace(/\s+/g, ' ')
+
+  if (key == 'Enter' && word) {
+    getData(word)
+  }
+}
+
+function handleChange (event) {
+  console.log(event)
+}
+
+function handleVolumn (event) {
+  console.log(event)
+  nodes.volumn.style.color = '#4B70E2'
+  audio.play()
+
+  setTimeout(() => {
+    nodes.volumn.style.color = '#a0a5a8'
+  }, 5000)
+}
+
+function handleRemove (event) {
+  console.log(event)
+  nodes.input.value = ''
+  nodes.input.focus()
+}
 
 async function getData (word) {
   nodes.dictionary.classList.remove('active')
@@ -26,15 +75,22 @@ async function getData (word) {
 }
 
 function setDictionary (result) {
-  console.log(result)
-  const definitions = result.meanings[0].definitions[0]
+  const definitions = result.meanings[0]?.definitions[0]
 
   nodes.dictionary.classList.add('active')
   nodes.word.innerText = result.word
-  nodes.phontetics.innerText = `${result.meanings[0].partOfSpeech} /${result.phonetics[1].text}/`
+  nodes.phontetics.innerText = `${result.meanings[0]?.partOfSpeech} /${result.phonetics[0]?.text}/`
   nodes.meaning.innerText = definitions.definition
   nodes.example.innerText = definitions.example
-  console.log(result.meanings[0].synonyms)
+
+  result.phonetics?.find(item => audio = new Audio(item.audio))
+  
+  if (result.meanings[0].synonyms) {
+    console.log(result.meanings[0].synonyms)
+    result.meanings[0].synonyms.forEach((item, index) => {
+      nodes.synonyms.querySelector('.list').innerHTML += `<li>${item}</li>`
+    })
+  }
 }
 
-getData('rabbit')
+init()
